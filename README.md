248f6fd1ef4b8ba51a166413238128dc4932fbc6
Mbaye DIOP <jobmbay@gmail.com>
2021-11-23 18:31:19 +0000

    Creation of library and use of antd-mobile

---
+++ README.md
@@ -0,0 +1,27 @@
+# @fieldify/antd-mobile
+
+Rendering Fieldify&#x27; Types Using Ant mobile Design Framework
+
+## Installation
+
+```sh
+npm install @fieldify/antd-mobile
+```
+
+## Usage
+
+```js
+import { multiply } from "@fieldify/antd-mobile";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ example/src/App.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { ScrollView, Text, TouchableWithoutFeedback, View, StyleSheet, Image } from 'react-native';
+import * as Linking from 'expo-linking';
+import { Button, Flex, WhiteSpace, WingBlank } from '@ant-design/react-native';
+// import { multiply } from '@fieldify/antd-mobile';
+export default function App() {
+  // const [result, setResult] = React.useState<number | undefined>();
+
+  const [isBuilderShown, setIsBuilderShown] = useState(true);
+  const [isFormShown, setIsFormShown] = useState(true);
+  const [isRenderShown, setIsRenderShown] = useState(false);
+
+  React.useEffect(() => {
+    // multiply(3, 7).then(setResult);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <WhiteSpace />
+      <WhiteSpace />
+      <WhiteSpace />
+      <ScrollView
+        style={{ flex: 1 }}
+        automaticallyAdjustContentInsets={false}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+      >
+        <WingBlank style={{ marginTop: 20, marginBottom: 5 }}>
+          <Flex>
+            <Flex.Item style={{ paddingLeft: 4, paddingRight: 4, flex: 1 }}>
+              <Image
+                style={[styles.tinyLogo, {flex: 1}]}
+                source={{
+                  uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD8AAAA/CAYAAABXXxDfAAACn0lEQVRoge2bv2sTYRjHv3epIq0WWwg4KFSxCA6NwSFToQ6Zg5NLKKj/geBgXZzs4CAuDtop5h9wlI4uxqVNKWL9GaHgEDUxTnrxXnnKE0nrXRvMk9zlnvczBe7y3vu5e+953vc9HgchFHP1kwAWAeQBXABwPOzcGNIEsA5gFUCpXElvB3XxH/lirk6SywCuAzg0QsJheABWACyVK+lm9zm75Iu5+hyApwBm4tBrYWoACuVKeqPT7F95Fn8OYDIBomG0AMx3bsCOPA/1tYQ+8b3QCMjSK+DygWUl4mDPu/TD4aj+ISHBrVcoCJ5xOZ1pEifId9HlPK6RPMlnlMpnSH4qBh2Jgik3fn0aHlZeK6rlx6QbvLjgYnI6dKXcN23P4MUzH96v/tsSl796awzO4Nx3+PjKw/Z703c74sN+0OKS2ICnFSuvFSuvFfE83wuNusGXz//3X983+NGQ6Uck8k/u/cbWuh/FpXcRybD3+5+ciWADnlasvFZsnpfEmIOXtafOOhifCL7vlAhqrw1a3wafEuhzlehVZuccjB8Nt5/NOLh0ObVvG2+qPh7cbEt2KxDxJ/92w/DzC2b6xP7ixMSx4eyI2ICnFSuvFSuvFfFUV7iWwunz4feUNiO+fzU4fCQ4ndEkafPlcNb64vL5K6kDZngO7t/w8G4z+kV9JF9snFQ8PuvYgKcVK68VKy9Ju4dluPczHnvX4nn+0Z02Zs6Fp7JWw+DTVjzkxXdyRgn177zQZ7+Ro0HyVaXyVZfLrzSySvIlrjzQBPmWXC64W1Em/5i8O9F+iauONECet9FJdVxpWOC6syTT4sLCJrrzPBfazSd4BNS6Cwqxd5LDB7IAHiYoCHrsk+0WJ3qtoqYipFGqxaGJG81fwquoAfwBWHur1tz2iAAAAAAASUVORK5CYII=',
+                }}
+              />
+            </Flex.Item>
+            <Flex.Item style={{ paddingLeft: 4, paddingRight: 4, flex: 6, paddingTop: 6, alignItems: 'left' }}>
+              <Text style={{fontSize: 25, fontWeight: 'bold', flex: 5}}>@Fieldify/antd-mobile</Text>
+            </Flex.Item>
+          </Flex>
+        </WingBlank>
+        <WingBlank style={{ marginBottom: 5 }}>
+          <Flex>
+            <Flex.Item style={{ paddingLeft: 4, paddingRight: 4, flex: 6, paddingTop: 6, alignItems: 'left' }}>
+              <Text><Text style={{color: '#5c38eb'}} onPress={() => Linking.openURL('https://github.com/mykiimike/fieldify')}>Fieldify Engine</Text> v1.2.3 / <Text style={{color: '#5c38eb'}} onPress={() => Linking.openURL('https://github.com/Fieldify/antd')}>Antd mobile</Text> v1.0.0</Text>
+            </Flex.Item>
+          </Flex>
+        </WingBlank>
+        <WhiteSpace />
+        <WingBlank style={{ marginBottom: 5 }}>
+          <Flex>
+            <Flex.Item style={{ paddingLeft: 4, paddingRight: 4 }}>
+              <Button style={{backgroundColor: isBuilderShown ? '#5c38eb' : '#ffffff'}} onPress={() => {
+                setIsBuilderShown(!isBuilderShown);
+              }} size="small"><Text style={{color: isBuilderShown ? 'white' : 'black'}}>Show Builder</Text></Button>
+            </Flex.Item>
+            <Flex.Item style={{ paddingLeft: 4, paddingRight: 4 }}>
+              <Button style={{backgroundColor: isFormShown ? '#5c38eb' : '#ffffff'}} onPress={() => {
+                setIsFormShown(!isFormShown);
+              }} size="small"><Text style={{color: isFormShown ? 'white' : 'black'}}>Show Form</Text></Button>
+            </Flex.Item>
+            <Flex.Item style={{ paddingLeft: 4, paddingRight: 4 }}>
+              <Button style={{backgroundColor: isRenderShown ? '#5c38eb' : '#ffffff'}} onPress={() => {
+                setIsRenderShown(!isRenderShown);
+              }} size="small"><Text style={{color: isRenderShown ? 'white' : 'black'}}>Show Render</Text></Button>
+            </Flex.Item>
+          </Flex>
+        </WingBlank>
+        <WhiteSpace />
+        <WhiteSpace />
+        <WhiteSpace />
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'space-around',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  }
+});
